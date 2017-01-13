258f9a16803407aa97bbd8eb75e81c1015511945
kj415j45 <919815238@qq.com>
2017-01-13 22:34:08 +0800

    更新表结构

--- install/SiteManager.sql
+++ install/SiteManager.sql
@@ -10,8 +10,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 --
 -- Database: `SiteManager`
 --
-CREATE DATABASE IF NOT EXISTS `SiteManager` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `SiteManager`;
 
 -- --------------------------------------------------------
 
@@ -19,9 +17,8 @@ USE `SiteManager`;
 -- 表的结构 `activities`
 --
 
-DROP TABLE IF EXISTS `activities`;
-CREATE TABLE IF NOT EXISTS `activities` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `activities` (
+  `id` int(11) NOT NULL,
   `activity_name` varchar(255) NOT NULL,
   `site_id` int(11) NOT NULL,
   `author_id` int(11) NOT NULL,
@@ -29,8 +26,7 @@ CREATE TABLE IF NOT EXISTS `activities` (
   `end_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `parters` text,
   `activity_describe` text,
-  `activity_note` text,
-  PRIMARY KEY (`id`)
+  `activity_note` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -39,17 +35,11 @@ CREATE TABLE IF NOT EXISTS `activities` (
 -- 表的结构 `sites`
 --
 
-DROP TABLE IF EXISTS `sites`;
-CREATE TABLE IF NOT EXISTS `sites` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `sites` (
+  `id` int(11) NOT NULL,
   `site_name` varchar(255) NOT NULL,
   `site_describe` text,
-  `site_picture` mediumblob,
-  `site_device` text,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`site_name`),
-  KEY `name_2` (`site_name`),
-  KEY `name_3` (`site_name`)
+  `site_device` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -58,24 +48,43 @@ CREATE TABLE IF NOT EXISTS `sites` (
 -- 表的结构 `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
   `username` char(16) NOT NULL,
   `password` char(32) NOT NULL,
   `usergroup` enum('admin','user') NOT NULL DEFAULT 'user',
   `regist_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `activities` text,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `id` (`id`),
-  KEY `username_2` (`username`)
+  `activities` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activities`
+--
+ALTER TABLE `activities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sites`
+--
+ALTER TABLE `sites`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`site_name`),
+  ADD KEY `name_2` (`site_name`),
+  ADD KEY `name_3` (`site_name`);
 
 --
 -- Indexes for table `users`
 --
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `username_2` (`username`);
 ALTER TABLE `users` ADD FULLTEXT KEY `username_3` (`username`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
