65f5e9c52a4b0852174ae89594625abdf820623a
kj415j45 <919815238@qq.com>
2017-01-13 12:59:40 +0800

    准备轮子

---
+++ install/SiteManager.sql
@@ -0,0 +1,83 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `SiteManager`
+--
+CREATE DATABASE IF NOT EXISTS `SiteManager` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `SiteManager`;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `activities`
+--
+
+DROP TABLE IF EXISTS `activities`;
+CREATE TABLE IF NOT EXISTS `activities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `activity_name` varchar(255) NOT NULL,
+  `site_id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `parters` text,
+  `activity_describe` text,
+  `activity_note` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `sites`
+--
+
+DROP TABLE IF EXISTS `sites`;
+CREATE TABLE IF NOT EXISTS `sites` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `site_name` varchar(255) NOT NULL,
+  `site_describe` text,
+  `site_picture` mediumblob,
+  `site_device` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`site_name`),
+  KEY `name_2` (`site_name`),
+  KEY `name_3` (`site_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` char(16) NOT NULL,
+  `password` char(32) NOT NULL,
+  `usergroup` enum('admin','user') NOT NULL DEFAULT 'user',
+  `regist_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `activities` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `id` (`id`),
+  KEY `username_2` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users` ADD FULLTEXT KEY `username_3` (`username`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
