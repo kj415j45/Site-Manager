71df635a0954254efd1d9dc43480f72dc6c85df6
kj415j45 <919815238@qq.com>
2017-01-15 15:05:50 +0800

    fix

--- include/header.html.php
+++ include/header.html.php
@@ -2,7 +2,7 @@
         <div class="container">
             <a class="navbar-brand" href="index.php"><?php echo $site_head ?></a>
             <ul class="nav navbar-nav">
-                <li <?php if(!$index_page) echo 'class="active"'?>><a href="index.php">主页</a></li>
+                <li <?php if($index_page) echo 'class="active"'?>><a href="index.php">主页</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <?php if(!$login_page) echo '<li><a href="login.php">登录</a></li>'?>
