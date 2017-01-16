6c62385550f021c4a587bdfbc28b7f1786bb509b
kj415j45 <919815238@qq.com>
2017-01-16 09:45:10 +0800

    fix and add TODO

--- include/header.html.php
+++ include/header.html.php
@@ -2,10 +2,10 @@
         <div class="container">
             <a class="navbar-brand" href="index.php"><?php echo $site_head ?></a>
             <ul class="nav navbar-nav">
-                <li <?php if($index_page) echo 'class="active"'?>><a href="index.php">主页</a></li>
+                <li <?php if($index_page){echo 'class="active"';}?>><a href="index.php">主页</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <?php if(!$login_page) echo '<li><a href="login.php">登录</a></li>'?>
+                <?php if(!$login_page){echo '<li><a href="login.php">登录</a></li>';}else{/*TODO 用户信息界面*/}?>
             </ul>
         </div>
     </nav>
