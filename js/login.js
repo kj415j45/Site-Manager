f49cfd77e9739dd0d1946c86bc308a4b06b817b7
kj415j45 <919815238@qq.com>
2017-01-18 17:15:53 +0800

    修复可以无视检查直接提交的BUG

--- js/login.js
+++ js/login.js
@@ -6,10 +6,10 @@ function username_check(){
 	if($("#username").val().match(check_username)!=$("#username").val()){
 		$("#u_n").addClass("has-error");
 		$("#notice").append('<div class="alert alert-danger text-center" role="alert">用户名必须字母开头，字母数字下划线组成，4-16个字符</div>');
-		return;
+		return false;
 	}
 	$("#u_n").addClass("has-success");
-	return;
+	return true;
 }
 function password_check(){
 	$("#u_p").removeClass("has-success");
@@ -20,33 +20,37 @@ function password_check(){
 	if($("#password").val().length<6||$("#password").val().length>16){
 		$("#u_p").addClass("has-error");
 		$("#notice").append('<div class="alert alert-danger text-center" role="alert">密码长度限制为6-16位</div>');
-		return;
+		return false;
 	}
 	if($("#password").val().match(check_password_easy_number)==$("#password").val()){
 		$("#u_p").addClass("has-error");
 		$("#notice").append('<div class="alert alert-danger text-center" role="alert">密码不应为纯数字</div>');
-		return;
+		return false;
 	}else if($("#password").val().match(check_password_easy_letter)==$("#password").val()){
 		$("#u_p").addClass("has-error");
 		$("#notice").append('<div class="alert alert-danger text-center" role="alert">密码不应为纯字母</div>');
-		return;
+		return false;
 	}
 	$("#u_p").addClass("has-success");
-	return;
+	return true;
 }
 function login(){
 	$("#notice").empty();
 	$("#notice").append('<div class="alert alert-info text-center" role="alert">登录中...</div>');
-    document.getElementById("md5password").value=hex_md5(document.getElementById("password").value);
-    document.getElementById("method").value="login";
-    document.getElementById("password").disabled=true;
-    document.login_form.submit();
+	if(username_check()&&password_check()){
+		document.getElementById("md5password").value=hex_md5(document.getElementById("password").value);
+		document.getElementById("method").value="login";
+		document.getElementById("password").disabled=true;
+		document.login_form.submit();
+	}
 }
 function regist(){
 	$("#notice").empty();
 	$("#notice").append('<div class="alert alert-info text-center" role="alert">注册中...</div>');
-    document.getElementById("md5password").value=hex_md5(document.getElementById("password").value);
-    document.getElementById("method").value="regist";
-    document.getElementById("password").disabled=true;
-    document.login_form.submit();
+	if(username_check()&&password_check()){
+		document.getElementById("md5password").value=hex_md5(document.getElementById("password").value);
+		document.getElementById("method").value="regist";
+		document.getElementById("password").disabled=true;
+		document.login_form.submit();
+	}
 }
