c058a0d1ae1a686abed152400b852aef3089fb16
MouseDevelop <937279156@163.com>
2017-01-20 19:35:00 +0800

    Update activities.html.php

--- activities.html.php
+++ activities.html.php
@@ -7,27 +7,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $site_name ?></title>
 
-    <!-- Bootstrap��ʽ�� -->
+    <!-- Bootstrap样式表 -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     
     <link href="css/style.css" rel="stylesheet">
   </head>
   <body>
-    <!-- ͷ����ʼ -->
+    <!-- 头部开始 -->
     <?php require "include/header.html.php"; ?>
-	<!-- ͷ������ -->
-	<!-- ��������ʼ -->
+	<!-- 头部结束 -->
+	<!-- 主容器开始 -->
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                    
+                    <h1>活动列表</h1>
+                    <table class="table table-condensed table-striped">
+                        <tbody>
+                            <tr>
+                                <td>ID</td>
+                                <td>活动名称</td>
+                                <td>发起人</td>
+                                <td>活动简介</td>
+                                <td>开始时间</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td><a href="index.php">膜法师大会</a></td>
+                                <td>江泽民</td>
+                                <td>我今天作为一个长者在这里...</td>
+                                <td>2017-01-27</td>
+                            </tr>
+                        </tbody>
+                    </table>
             </div>
         </div>
     </div>
-    <!-- ���������� -->
-    <!-- �ײ���ʼ -->
+    <!-- 主容器结束 -->
+    <!-- 底部开始 -->
     <?php require "include/footer.html.php" ?>
-    <!-- �ײ����� -->
+    <!-- 底部结束 -->
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -35,9 +53,9 @@
       <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 	
-	<!-- ����jQuery(Bootstrap����ǰ�ü�jQ�����������) -->
+	<!-- 导入jQuery(Bootstrap所需前置及jQ插件所需依赖) -->
     <script src="js/jquery.min.js"></script>
-    <!-- ����Bootstrap -->
+    <!-- 导入Bootstrap -->
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
