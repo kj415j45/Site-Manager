b5943d638fa673f19361a2dfc3b84ce19fad854d
kj415j45 <919815238@qq.com>
2017-01-21 10:26:07 +0800

    修复SQL字符集问题

--- include/sql_connect.php
+++ include/sql_connect.php
@@ -2,4 +2,5 @@
     require_once __DIR__."/config.php";
     require __DIR__."/../class/SQL.class.php";
     SQL::SQLConnect($sql_host,$sql_username,$sql_password,"SiteManager");
+	SQL::query("SET NAMES utf8");
 ?>
