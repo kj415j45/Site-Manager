dde756d4aa536d15c9932bc77897a089d0f33504
kj415j45 <919815238@qq.com>
2017-01-24 16:22:48 +0800

    添加一张表

--- install/SiteManager.sql
+++ install/SiteManager.sql
@@ -1,24 +1,15 @@
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `SiteManager`
---
+CREATE DATABASE IF NOT EXISTS `SiteManager` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `SiteManager`;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `activities`
---
-
-CREATE TABLE `activities` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `activities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `activity_name` varchar(255) NOT NULL,
   `site_id` int(11) NOT NULL,
   `author_id` int(11) NOT NULL,
@@ -26,65 +17,43 @@ CREATE TABLE `activities` (
   `end_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `parters` text,
   `activity_describe` text,
-  `activity_note` text
+  `activity_note` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `sites`
---
-
-CREATE TABLE `sites` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `sites` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `site_name` varchar(255) NOT NULL,
   `site_describe` text,
-  `site_device` text
+  `site_device` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`site_name`),
+  KEY `name_2` (`site_name`),
+  KEY `name_3` (`site_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `userinfo` (
+  `id` int(11) NOT NULL,
+  `name` char(32) NOT NULL,
+  `telephone` char(11) NOT NULL,
+  `qq` char(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- 表的结构 `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` char(16) NOT NULL,
   `password` char(32) NOT NULL,
   `usergroup` enum('admin','user') NOT NULL DEFAULT 'user',
   `regist_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `activities` text
+  `activities` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `id` (`id`),
+  KEY `username_2` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `activities`
---
-ALTER TABLE `activities`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `sites`
---
-ALTER TABLE `sites`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`site_name`),
-  ADD KEY `name_2` (`site_name`),
-  ADD KEY `name_3` (`site_name`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `username_2` (`username`);
 ALTER TABLE `users` ADD FULLTEXT KEY `username_3` (`username`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
