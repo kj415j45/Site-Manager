2986e89f18b47dc7fbbdad6c4818313366778264
kj415j45 <919815238@qq.com>
2017-02-16 17:52:54 +0800

    爆炸中，无法拉取全部场地名

--- include/get_sites.php
+++ include/get_sites.php
@@ -2,4 +2,11 @@
 	session_start();
 	require_once __DIR__."/sql_connect.php";
 	require_once __DIR__."/public_function.php";
+	
+	SQL::query("SELECT site_name FROM sites");
+	$assoc=mysqli_fetch_assoc(SQL::getResult());
+	show($assoc);
+	foreach($assoc as $site){
+		echo '<option>'.$site.'</option>';
+	}
 ?>
