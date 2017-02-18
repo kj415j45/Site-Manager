5b2c1290410f6b6031d4587ce688c61e9dfdc3a3
Mouse <937279156@qq.com>
2017-02-18 18:14:56 +0800

    Update header fix #7

--- include/userinfo.html.php
+++ include/userinfo.html.php
@@ -2,5 +2,12 @@
 <li><a href="#"><?php echo $_SESSION["usergroup"]; ?></a></li>
 <li><a href="#"><?php echo $_SESSION["username"]; ?></a></li>
 <li><a href="activities.php?user=<?php echo $_SESSION["username"]; ?>">我的活动</a></li>
+<?php 
+	if($_SESSION["usergroup"]=="管理员"){
+?>
+	<li><a href="admin.php">后台管理</a></li>
+<?php
+	}
+?>
 <li><a href="login.php?action=logout">退出</a></li>
-<li><img src="userhead/<?php echo $_SESSION["username"] ?>.jpg" class="img-circle" width=48 height=48></li>
+<li class="hidden-xs"><img src="userhead/<?php echo $_SESSION["username"] ?>.jpg" class="img-circle" width=48 height=48></li>
