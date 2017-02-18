cc973a253cbe0945d5a2285101de45b0f38e1351
Mouse <937279156@qq.com>
2017-02-18 18:32:38 +0800

    Fixed footer bug fix #9

--- include/footer.html.php
+++ include/footer.html.php
@@ -1,5 +1,7 @@
-<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
-    <div class="container">
-        <?php echo $site_foot ?>
-    </div>
-</nav>
+<div style="margin-top:65px;">
+	<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+		<div class="container">
+			<?php echo $site_foot ?>
+		</div>
+	</nav>
+</div>
