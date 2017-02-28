a684a5c2a5b37c93443ef26ef724a6462369d5e5
Mouse <937279156@qq.com>
2017-02-28 18:00:53 +0800

    Update admin

---
+++ admin.html.php
@@ -0,0 +1,69 @@
+<?php require_once "include/config.php"?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $site_name ?></title>
+
+    <!-- Bootstrap样式表 -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <!-- 头部开始 -->
+	<?php session_start();?>
+	<nav class="navbar navbar-default navbar-static-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php"><?php echo $site_head ?></a>
+				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="true">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<nav id="bs-navbar" class="navbar-collapse collapse" aria-expanded="true">
+				<ul class="nav navbar-nav">
+					<li><a>管理活动申请</a></li>
+					<li><a>管理用户申请</a></li>
+					<li><a>活动管理</a></li>
+					<li><a>场地管理</a></li>
+					<li><a>用户管理</a></li>
+					<li><a>全局设置</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="index.php">回到主页</a></li>
+				</ul>
+			</nav>
+		</div>
+	</nav>
+	<!-- 头部结束 -->
+	<!-- 主容器开始 -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                 233
+            </div>
+        </div>
+    </div>
+    <!-- 主容器结束 -->
+    <!-- 底部开始 -->
+    <?php require "include/footer.html.php" ?>
+    <!-- 底部结束 -->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+	<!-- 导入jQuery(Bootstrap所需前置及jQ插件所需依赖) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- 导入Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
