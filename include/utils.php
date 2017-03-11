c6b90cdc1d460c42b1e9c2c75cbd7bf60fcf4432
kj415j45 <kj415j45@gmail.com>
2017-03-12 00:05:20 +0800

    fix typo

--- include/utils.php
+++ include/utils.php
@@ -1,26 +1,26 @@
-<?php	
+<?php
 require_once __DIR__."/sql_connect.php";
 function getActivities(){
 		SQL::SELECT("activity_name,username,activity_describe,activity_note,start_time,end_time,site_name",
 					"activities,users,sites",
 					"activities.author_id = users.id AND site_id = sites.id");
-		while($row=SQL::getResult()->fetch_assoc()){    
+		while($row=SQL::getResult()->fetch_assoc()){
 			$assoc[]=$row;
 		}
 		return $assoc;
 }
 
-function getActivitiesFromUser($username){
+function getActivitiesByUser($username){
 		SQL::SELECT("activity_name,username,activity_describe,activity_note,start_time,end_time,site_name",
 					"activities,users,sites",
 					"activities.author_id = '".$username."'AND activities.author_id = users.id AND site_id = sites.id");
-		while($row=SQL::getResult()->fetch_assoc()){    
+		while($row=SQL::getResult()->fetch_assoc()){
 			$assoc[]=$row;
 		}
 		return $assoc;
 }
 
-function getActivitiesFromID($id){
+function getActivitiesByID($id){
 		SQL::SELECT("activity_name,username,activity_describe,activity_note,start_time,end_time,site_name",
 					"activities,users,sites",
 					"activities.id = '".$id."'AND activities.author_id = users.id AND site_id = sites.id");
