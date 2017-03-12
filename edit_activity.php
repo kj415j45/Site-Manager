381b08fc61c114f4c2f43f3bbb524b26704c6910
kj415j45 <kj415j45@gmail.com>
2017-03-12 22:00:20 +0800

    finish edit activity

--- edit_activity.php
+++ edit_activity.php
@@ -6,11 +6,52 @@
 	unset($assoc);
 	
 	if(isset($_POST["activity_id"])&&$_POST["activity_id"]!='0'){
+	    $start_time=strtotime($_POST["start_time"]);
+		$end_time=strtotime($_POST["end_time"]);
+		$now=time();
+	    if($start_time>=$end_time||$start_time<$now){
+	        js_message('不合法的时间');
+	        page_jump('edit_activity.php?id='.$_POST["activity_id"],0);
+	    }
+	    
+	    $start_time=date("YmdHis",$start_time);
+	    $end_time=date("YmdHis",$end_time);
+	    
+	    SQL::SELECT("id,activity_name",
+	                "activities",
+	                "NOT( (start_time<'{$start_time}' AND end_time<'{$start_time}') OR (start_time>'{$end_time}' AND end_time>'{$end_time}') ) AND site_id=(SELECT id FROM sites WHERE site_name='{$_POST['site_name']}')",
+	                "LIMIT 1");
+	    $x=SQL::getAssoc(SQL::getResult());
+	    if($x!=NULL){
+	        js_message('与id为'.$x['id'].'的活动"'.$x['activity_name'].'"有时间冲突');
+	        page_jump('edit_activity.php?id='.$_POST["activity_id"],0);
+	    }
 		SQL::UPDATE("activities,sites",
 					"site_id=sites.id,activity_name='".$_POST["activity_name"]."',start_time='".$_POST["start_time"]."',end_time='".$_POST["end_time"]."',activity_describe='".$_POST["activity_describe"]."'",
 					"activities.id='".$_POST["activity_id"]."' AND sites.site_name='".$_POST["site_name"]."';");//不考虑恶意修改(不验证session)
 		page_jump("activity.php?id=".$_POST["activity_id"],0);
 	}else if($_POST["activity_id"]=='0'){
+	    $start_time=strtotime($_POST["start_time"]);
+		$end_time=strtotime($_POST["end_time"]);
+		$now=time();
+	    if($start_time>=$end_time||$start_time<$now){
+	        js_message('不合法的时间');
+	        page_jump('edit_activity.php?id='.$_POST["activity_id"],0);
+	    }
+	    
+	    $start_time=date("YmdHis",$start_time);
+	    $end_time=date("YmdHis",$end_time);
+	    
+	    SQL::SELECT("id,activity_name",
+	                "activities",
+	                "NOT( (start_time<'{$start_time}' AND end_time<'{$start_time}') OR (start_time>'{$end_time}' AND end_time>'{$end_time}') ) AND site_id=(SELECT id FROM sites WHERE site_name='{$_POST['site_name']}')",
+	                "LIMIT 1");
+	    $x=SQL::getAssoc(SQL::getResult());
+	    if($x!=NULL){
+	        js_message('与id为'.$x['id'].'的活动"'.$x['activity_name'].'"有时间冲突');
+	        page_jump('edit_activity.php?id='.$_POST["activity_id"],0);
+	    }
+	    
 		SQL::SELECT("id",
 					"sites",
 					"site_name='{$_POST['site_name']}'");
