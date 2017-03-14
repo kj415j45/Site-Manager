bc6baff66b15833e18153b534e6ae5a0f5d0a2ce
Mouse <937279156@qq.com>
2017-03-14 17:27:35 +0800

    场地

---
+++ site.html.php
@@ -0,0 +1,50 @@
+<?php require_once "include/config.php"?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?=$site_name ?></title>
+
+    <!-- Bootstrap样式表 -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <!-- 头部开始 -->
+    <?php require "include/header.html.php"; ?>
+	<!-- 头部结束 -->
+	<!-- 主容器开始 -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10">
+				<h1 class="page-header">场地:test</h1>
+			</div>
+			<div class="col-md-2 page-header"></div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">详细介绍</div>
+			<div class="panel-body">
+				<p>test</p>
+			</div>
+		</div>
+    </div>
+    <!-- 主容器结束 -->
+    <!-- 底部开始 -->
+    <?php require "include/footer.html.php" ?>
+    <!-- 底部结束 -->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+	<!-- 导入jQuery(Bootstrap所需前置及jQ插件所需依赖) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- 导入Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
