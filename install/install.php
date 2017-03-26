ab6f72a0614cfaade9176624b440b8598206e4a1
kj415j45 <kj415j45@gmail.com>
2017-03-26 13:38:30 +0800

    install script finish

--- install/install.php
+++ install/install.php
@@ -1,13 +1,32 @@
 <?php
+    require_once "../include/public_function.php";
     if(file_exists('.install_lock')){
         exit(0);
-    }else{
-        $mysql=mysqli_connect($_POST["sql_host"],$_POST["sql_user"],$_POST["sql_password"]);
-        mysqli_query($mysql,"source 'SiteManager.sql';");
+    }else if(isset($_POST["sql_host"])){
+        $mysql=mysqli_connect($_POST["sql_host"],$_POST["sql_username"],$_POST["sql_password"]);
+        $fp=fopen("SiteManager.sql","r");
+        $sql=fread($fp,filesize("SiteManager.sql"));
+        fclose($fp);
+        $sql=explode(";",$sql);
+        for($i=0;$i<count($sql);$i++){
+            mysqli_query($mysql,$sql[$i]);
+        }
         mysqli_close($mysql);
+        $config=fopen("../include/config.php","x");
+        fwrite($config,"<?php\n\$sql_host=\"{$_POST['sql_host']}\";//mysql数据库地址\n\$sql_username=\"{$_POST['sql_username']}\";//mysql用户名\n\$sql_password=\"{$_POST['sql_password']}\";//mysql密码\n\$site_host=\"{$_POST['site_host']}\";//网站域名(结尾带'/')\n\$site_name=\"{$_POST['site_name']}\";//网站标题\n\$site_head=\"{$_POST['site_head']}\";//顶部横幅\n\$site_foot=\"{$_POST['site_foot']}\";//底部信息");
+        fclose($config);
+        require_once "../include/sql_connect.php";
+        SQL::INSERT("users",
+					"username,password,usergroup,regist_time,last_time",
+					"'{$_POST["admin_username"]}','{$_POST["md5password"]}','admin','".date("YmdHis",time())."','".date("YmdHis",time())."'");
+        SQL::INSERT("userinfo",
+			            "name",
+			            "NULL");
         $lock=fopen(".install_lock","x");
         fwrite($lock,"install_lock");
         fclose($lock);
+        js_message("安装成功");
+        page_jump($site_host,0);
     }
 ?>
 <!DOCTYPE html>
@@ -31,21 +50,27 @@
                     <form class="form-horizontal" id="install_form" name="install_form" onsubmit="return false;" method="post" action="install.php">
                         <div class="form-group">
                             <label class="control-label">数据库地址</label>
-                                <input type="text" class="form-control" name="sql_host" placeholder="localhost" value="localhost">
+                                <input type="text" class="form-control" name="sql_host" placeholder="" value="localhost">
                             <label class="control-label">数据库用户</label>
-                                <input type="text" class="form-control" name="sql_user" placeholder="root" value="root">
+                                <input type="text" class="form-control" name="sql_username" placeholder="" value="root">
                             <label class="control-label">数据库密码</label>
                                 <input type="password" class="form-control" name="sql_password" placeholder="" value="">
                         </div>
                         <div class="form-group">
-                            <div class="input-group">
+                            <label class="control-label">访问时域名</label>
+                                <input type="text" class="form-control" name="site_host" placeholder="" value="http://localhost/">
+                            <label class="control-label">网站标题</label>
+                                <input type="text" class="form-control" name="site_name" placeholder="" value="场地管理">
+                            <label class="control-label">头部横幅</label>
+                                <input type="text" class="form-control" name="site_head" placeholder="" value="场地管理">
+                            <label class="control-label">底部信息</label>
+                                <input type="text" class="form-control" name="site_foot" placeholder="" value="">
+                        </div>
+                        <div class="form-group">
                             <label class="control-label">管理员用户名</label>
-                                <input type="text" class="form-control" name="admin_user" placeholder="root" value="root">
-                            </div>
-                            <div class="input-group">
+                                <input type="text" class="form-control" name="admin_username" placeholder="root" value="root">
                             <label class="control-label">管理员密码</label>
-                                <input type="password" class="form-control" name="admin_password" id="admin_password" placeholder="" value="">
-                            </div>
+                                <input type="password" class="form-control" id="admin_password" placeholder="" value="">
                         </div>
                         <div class="form-group">
                             <input type="hidden" name="md5password" id="md5password">
